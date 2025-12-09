3fcda1789e52d29c5537671b5b604c242f4474ee
Benjamin Canac <canacb1@gmail.com>
2025-12-09 22:03:14 +0100

    feat(app): add error page

---
+++ app/error.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps<{
+  error: NuxtError
+}>()
+</script>
+
+<template>
+  <div class="flex flex-col h-screen">
+    <div class="flex-1 flex flex-col items-center justify-center gap-8 rounded-md ring ring-default bg-default/75 shadow m-4">
+      <UError :error="error" class="min-h-0" />
+    </div>
+  </div>
+</template>
