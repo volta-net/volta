213ced7c2f7d9b4bd0f8e0da2688fa097dad5ec4
Benjamin Canac <canacb1@gmail.com>
2025-12-10 21:25:32 +0100

    chore(app): improve head

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import colors from 'tailwindcss/colors'
+
 const { user } = useUserSession()
+
+const colorMode = useColorMode()
+const appConfig = useAppConfig()
+
+const color = computed(() => colorMode.value === 'dark' ? (colors as any)[appConfig.ui.colors.neutral][900] : 'white')
 
 useHead({
   meta: [
-    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { key: 'theme-color', name: 'theme-color', content: color }
   ],
   link: [
     { rel: 'icon', href: '/favicon.ico' }
@@ -13,16 +21,14 @@ useHead({
   }
 })
 
-const title = 'Nuxt Starter Template'
-const description = 'A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours.'
+const title = 'Volta'
+const description = 'A better GitHub inbox for busy maintainers.'
 
 useSeoMeta({
   title,
   description,
   ogTitle: title,
   ogDescription: description,
-  ogImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
-  twitterImage: 'https://ui.nuxt.com/assets/templates/nuxt/starter-light.png',
   twitterCard: 'summary_large_image'
 })
 </script>
@@ -40,7 +46,7 @@ useSeoMeta({
         <AppLogo class="mx-auto h-12 text-highlighted" />
 
         <h1 class="text-lg font-medium text-toned">
-          A better GitHub inbox for busy maintainers.
+          {{ description }}
         </h1>
 
         <UButton
