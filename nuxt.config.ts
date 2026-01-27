ac926269d7abdeddf3237f7c62d0c46d21cbd432
Sébastien Chopin <atinux@gmail.com>
2026-01-27 13:53:40 +0100

    chore: update back compat date

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -84,8 +84,7 @@ export default defineNuxtConfig({
     asyncContext: true
   },
 
-  // compatibilityDate: '2026-01-15',
-  compatibilityDate: '2025-01-15',
+  compatibilityDate: '2026-01-15',
 
   hub: {
     db: 'postgresql'
